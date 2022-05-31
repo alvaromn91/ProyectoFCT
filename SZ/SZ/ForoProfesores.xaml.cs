1709f471c8e01a401aadfeb4a34251db7ce3a92b
Alejandro Velasco Guerrero <alejandro.velasco.guerrero@alumnojoyfe.iepgroup.es>
2022-05-31 20:44:03 +0200

    ChatServer V0.1

---
+++ SZ/SZ/ForoProfesores.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SZ
+{
+    /// <summary>
+    /// Lógica de interacción para ForoProfesores.xaml
+    /// </summary>
+    public partial class ForoProfesores : Window
+    {
+        public ForoProfesores()
+        {
+            InitializeComponent();
+        }
+    }
+}
